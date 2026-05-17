eb6b4a2441fc696948f0bb9f1490dc579f6fb167
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-05-17 15:06:13 +0300

    Add Koin configuration for IOS entry point.

--- composeApp/src/androidMain/kotlin/com/example/cargo_customer/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/cargo_customer/Platform.android.kt
@@ -1,9 +1,19 @@
 package com.example.cargo_customer
 
 import android.os.Build
+import com.cargo.customer.shared.di.koinModule
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual fun initKoin(koinDeclaration: KoinAppDeclaration?) {
+    startKoin {
+        koinDeclaration?.invoke(this)
+        modules(koinModule)
+    }
+}
--- composeApp/src/androidMain/kotlin/com/example/cargo_customer/app/CargoApplication.kt
+++ composeApp/src/androidMain/kotlin/com/example/cargo_customer/app/CargoApplication.kt
@@ -2,17 +2,18 @@ package com.example.cargo_customer.app
 
 import android.app.Application
 import com.cargo.customer.shared.di.koinModule
+import com.example.cargo_customer.initKoin
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.GlobalContext.startKoin
+import org.koin.android.ext.koin.androidLogger
 
 
 class CargoApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        println("Application")
-        startKoin {
+        initKoin {
             androidContext(this@CargoApplication)
+            androidLogger()
             modules(koinModule)
         }
     }

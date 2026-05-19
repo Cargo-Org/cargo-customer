2ef0551c639c37c33dffd6c564088a00e0473cf6
Abdallah-Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-05-19 06:06:51 +0300

    feat: add verify email screen with OTP

--- composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
@@ -3,12 +3,12 @@ package com.example.cargo_customer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.cargo_customer.presentation.register.RegisterScreen
+import com.example.cargo_customer.presentation.verify_email.EmailVerificationScreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        RegisterScreen()
+        EmailVerificationScreen()
     }
 }

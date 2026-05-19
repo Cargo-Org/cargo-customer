6514165a0e7141c8dec72b570d2c2144d6dc3b5e
Hendsayed25 <hendsayed853@gmail.com>
2026-05-19 09:40:39 +0300

    Remove unused RegisterScreen import.

--- composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cargo_customer.presentation.navigation.NavGraph
-import com.example.cargo_customer.presentation.register.RegisterScreen
 
 @Composable
 @Preview

72dbc513bbd0dabf44100e9fab9e4f83f3ce0cf7
Abdallah-Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-05-20 09:05:25 +0300

    refactor(app): integrate redesigned application theme

--- composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
@@ -1,17 +1,12 @@
 package com.example.cargo_customer
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cargo_customer.presentation.navigation.NavGraph
-import com.example.cargo_customer.presentation.navigation.Route
+import com.example.cargo_customer.presentation.theme.CargoCustomerTheme
+import com.example.cargo_customer.presentation.verify_email.EmailVerificationScreen
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        NavGraph(
-            startDestination = Route.LoginRoute //this will change according to start screen
-        )
+    CargoCustomerTheme {
+        EmailVerificationScreen()
     }
 }

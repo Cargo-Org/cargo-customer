1220cb1171108a165973f4991ade3cacf34d9393
Abdallah-Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-05-22 11:55:32 +0300

    Resolve conflicts and migrate screens to new presentation/screen structure

--- composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cargo_customer/App.kt
@@ -1,13 +1,15 @@
 package com.example.cargo_customer
 
 import androidx.compose.runtime.*
-import com.example.cargo_customer.presentation.theme.CargoCustomerTheme
 import com.example.cargo_customer.presentation.navigation.NavGraph
 import com.example.cargo_customer.presentation.navigation.Route
+import com.example.cargo_customer.presentation.theme.CargoCustomerTheme
 
 @Composable
 fun App() {
     CargoCustomerTheme {
-        EmailVerificationScreen()
+        NavGraph(
+            startDestination = Route.LoginRoute //this will change according to start screen
+        )
     }
 }

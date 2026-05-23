ccab829576869f45973c131a7ad1420702689f81
ZEIAD-LAPTOP\zeiad <zazaoskar928@gmail.com>
2026-05-23 22:01:21 +0300

    Add interactive NationalIDCard, fluid stepper, and supporting document upload UI components

---
+++ composeApp/src/commonMain/kotlin/com/example/cargo_customer/presentation/screen/upload_docs/component/TipRow.kt
@@ -0,0 +1,43 @@
+package com.example.cargo_customer.presentation.screen.upload_docs.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.example.cargo_customer.presentation.theme.CargoTheme
+
+@Composable
+fun TipRow(
+    text: String,
+    textColor: Color = CargoTheme.colorScheme.onBackground,
+    icon: ImageVector,
+    iconColor: Color = CargoTheme.colorScheme.primary,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically, modifier = modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .size(CargoTheme.dimens.sizing.iconXl)
+                .background(color = iconColor.copy(alpha = 0.2F), shape = CargoTheme.shapes.small)
+                .padding(CargoTheme.dimens.spacing.sm),
+            contentAlignment = Alignment.Center)
+        {
+            Image(icon, contentDescription = null, colorFilter = ColorFilter.tint(iconColor))
+        }
+
+        Spacer(modifier = Modifier.width(CargoTheme.dimens.spacing.lg))
+
+        Text(
+            text = text, color = textColor, style = CargoTheme.typography.labelMedium
+        )
+    }
+}

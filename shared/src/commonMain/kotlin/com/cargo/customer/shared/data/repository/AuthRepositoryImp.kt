32e4bcd1f504f796db0acec3b90db89b19a654e5
Hendsayed25 <hendsayed853@gmail.com>
2026-06-04 15:54:22 +0300

    implement document upload and retrieval in AuthRepository

--- shared/src/commonMain/kotlin/com/cargo/customer/shared/data/repository/AuthRepositoryImp.kt
+++ shared/src/commonMain/kotlin/com/cargo/customer/shared/data/repository/AuthRepositoryImp.kt
@@ -1,11 +1,49 @@
 package com.cargo.customer.shared.data.repository
 
 import com.cargo.customer.shared.data.local.datasource.AuthLocalDataSource
+import com.cargo.customer.shared.data.mapper.toDomain
 import com.cargo.customer.shared.data.remote.datasource.AuthRemoteDataSource
+import com.cargo.customer.shared.domain.model.DocumentModel
+import com.cargo.customer.shared.domain.model.UploadUrlModel
 import com.cargo.customer.shared.domain.repository.AuthRepository
+import com.cargo.customer.shared.domain.result.ApiResult
 
 class AuthRepositoryImp (
     private val remote: AuthRemoteDataSource,
     private val local: AuthLocalDataSource
 ): AuthRepository {
+
+    override suspend fun getUploadUrl(): ApiResult<UploadUrlModel> {
+        return when(val result = remote.getUploadUrl()) {
+            is ApiResult.Success -> { ApiResult.Success(result.data.toDomain()) }
+            is ApiResult.Error -> result
+        }
+    }
+
+    override suspend fun uploadImageToUrl(uploadUrl: String, bytes: ByteArray): ApiResult<Unit> {
+        return when(val result = remote.uploadImageToUrl(uploadUrl, bytes)) {
+            is ApiResult.Success -> { ApiResult.Success(Unit) }
+            is ApiResult.Error -> result
+        }
+    }
+
+    override suspend fun submitDocumentData(
+        documentType: Int,
+        fileName: String,
+        key: String,
+        contentType: String,
+        fileSizeInByte: String
+    ): ApiResult<Unit> {
+        return when(val result = remote.submitDocumentData(documentType, fileName, key,contentType ,fileSizeInByte)) {
+            is ApiResult.Success -> { ApiResult.Success(Unit) }
+            is ApiResult.Error -> result
+        }
+    }
+
+    override suspend fun getDocuments(): ApiResult<List<DocumentModel>> {
+        return when(val result = remote.getDocuments()) {
+            is ApiResult.Success -> { ApiResult.Success(result.data.map { it.toDomain() }) }
+            is ApiResult.Error -> result
+        }
+    }
 }
--- shared/src/commonMain/kotlin/com/cargo/customer/shared/domain/repository/AuthRepository.kt
+++ shared/src/commonMain/kotlin/com/cargo/customer/shared/domain/repository/AuthRepository.kt
@@ -1,4 +1,21 @@
 package com.cargo.customer.shared.domain.repository
 
+import com.cargo.customer.shared.domain.model.DocumentModel
+import com.cargo.customer.shared.domain.model.UploadUrlModel
+import com.cargo.customer.shared.domain.result.ApiResult
+
 interface AuthRepository {
+    suspend fun getUploadUrl(): ApiResult<UploadUrlModel>
+    suspend fun uploadImageToUrl(
+        uploadUrl: String,
+        bytes: ByteArray
+    ): ApiResult<Unit>
+    suspend fun submitDocumentData(
+        documentType: Int,
+        fileName: String,
+        key : String,
+        contentType: String,
+        fileSizeInByte: String
+    ): ApiResult<Unit>
+    suspend fun getDocuments(): ApiResult<List<DocumentModel>>
 }

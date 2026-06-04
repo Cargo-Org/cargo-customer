08b46698d9d2d31896f48f59e116c4fd5f251ab8
esraaehab333 <esraaehabb333@gmail.com>
2026-06-04 03:52:31 +0300

    solve : platform.ios.kt comflict

--- composeApp/src/iosMain/kotlin/com/example/cargo_customer/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/cargo_customer/Platform.ios.kt
@@ -1,11 +1,7 @@
 package com.example.cargo_customer
 
 import com.cargo.customer.shared.di.iosModules
-<<<<<<< HEAD
 import com.example.cargo_customer.presentation.di.presentationModule
-=======
-import com.example.cargo_customer.di.presentationModule
->>>>>>> feature/auth-register
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import platform.UIKit.UIDevice

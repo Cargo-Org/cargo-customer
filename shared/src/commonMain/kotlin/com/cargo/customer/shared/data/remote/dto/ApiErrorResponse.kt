3f0e9a2458d82c6375139e72ca6edf2782a4d16f
Abdallah-Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-06-06 19:12:08 +0300

    refactor(api): add explicit serialization field mappings

--- shared/src/commonMain/kotlin/com/cargo/customer/shared/data/remote/dto/ApiErrorResponse.kt
+++ shared/src/commonMain/kotlin/com/cargo/customer/shared/data/remote/dto/ApiErrorResponse.kt
@@ -1,19 +1,26 @@
 package com.cargo.customer.shared.data.remote.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 sealed interface ErrorResponse
 
 @Serializable
 data class ApiErrorResponse(
+    @SerialName("type")
     val type: String,
+    @SerialName("title")
     val title: String,
+    @SerialName("status")
     val status: Int,
+    @SerialName("detail")
     val detail: String,
 ): ErrorResponse
 
 @Serializable
 data class ValidationErrorResponse(
+    @SerialName("status")
     val status: Int,
+    @SerialName("errors")
     val errors: Map<String, List<String>>,
 ) : ErrorResponse

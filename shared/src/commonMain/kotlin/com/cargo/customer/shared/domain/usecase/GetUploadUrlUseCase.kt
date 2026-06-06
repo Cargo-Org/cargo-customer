3dff6fe81771ccd0513d385ec9351e5bdb609836
Hendsayed25 <hendsayed853@gmail.com>
2026-06-06 20:48:27 +0300

    Update GetUploadUrlUseCase to accept documentType and contentType parameters.

--- shared/src/commonMain/kotlin/com/cargo/customer/shared/domain/usecase/GetUploadUrlUseCase.kt
+++ shared/src/commonMain/kotlin/com/cargo/customer/shared/domain/usecase/GetUploadUrlUseCase.kt
@@ -5,5 +5,5 @@ import com.cargo.customer.shared.domain.repository.AuthRepository
 class GetUploadUrlUseCase(
     private val authRepository: AuthRepository
 ) {
-    suspend operator fun invoke() = authRepository.getUploadUrl()
+    suspend operator fun invoke(documentType:Int,contentType:String) = authRepository.getUploadUrl(documentType,contentType)
 }
